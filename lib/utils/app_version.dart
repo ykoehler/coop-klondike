c03e0c15daadc99a20069e6798271bf9a9137b4f
Yannick Koehler <ykoehler@gmail.com>
2025-10-05 19:47:36 -0400

    chore(version): update version string to v251005193813

--- lib/utils/app_version.dart
+++ lib/utils/app_version.dart
@@ -1,5 +1,6 @@
 /// Auto-generated file containing version information for asset paths
 /// This file is generated during build to ensure proper cache busting
+library;
 
 class AppVersion {
   static const String version = 'v251005182751';
